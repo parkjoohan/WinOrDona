30e88bdabdc975f1968ebecce4b9eb9a41e17751
alex0317@naver.com <joohanpark1513@gmail.com>
2023-03-16 08:39:49 +0900

    회원 기능 트랜잭션 처리

--- backend/donation/src/main/java/com/example/donation/service/user/UserService.java
+++ backend/donation/src/main/java/com/example/donation/service/user/UserService.java
@@ -7,6 +7,7 @@ import com.example.donation.dto.user.request.UserUpdateRequest;
 import com.example.donation.dto.user.response.UserResponse;
 import com.example.donation.repository.user.UserJdbcRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,10 +21,15 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    // 아래 있는 함수가 시작될 떄, start transaction을 해준다 (트랜잭션 시작)
+    // 점수가 예외 없이 잘 끝냈다면 commit
+    // 혹시라도 문제가 있다면 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         userRepository.save(new User(request.getLoginId(), request.getPassword(), request.getName()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         List<User> users = userRepository.findAll();
         return users.stream()
@@ -31,13 +37,17 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId()).orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.getName());
-        userRepository.save(user);
+
+        // 영속성 컨텍스트의 변경 감지 기능으로 save 자동 실행
+        // userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name)
                 .orElseThrow(IllegalArgumentException::new);
